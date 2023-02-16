8e42b3af85df206dcbbe1c1befb97d7014ac5349
Sarwesh2003 <sarweshkhairnar@gmail.com>
2023-02-16 17:04:10 +0530

    Changes

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import requests
 import json
 import pandas as pd
 import numpy as np
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -276,4 +277,4 @@ def predict():
   
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, port = port)
+    app.run(host = '0.0.0.0', port = port)
